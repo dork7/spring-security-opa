96315c68e32913d65b74c08d12a6fe80977ba01d
dork7 <hamzameh122@gmail.com>
2020-08-20 16:06:40 +0500

    office work

--- opa-commands/policy.sh
+++ opa-commands/policy.sh
@@ -1,3 +1,3 @@
-curl --location --request PUT '192.168.99.100:8181/v1/policies/opaweb/authz' \
+curl --location --request PUT 'localhost:8181/v1/policies/opaweb/authz' \
 --header 'Content-Type: text/plain' \
 --data-binary @opaweb-policy.rego
